263481099aaf6fa0f4e209cf60b59494c0ad613f
everinurmind <nurbol.sultanov@gmail.com>
2023-06-14 10:51:51 -0600

    [2pt] Preserve Data (Team member #1)
    [2pt] The following options should be available: (Team member #1)
    [2pt] Preserve Data (Team member #1) #9

--- main.rb
+++ main.rb
@@ -1,40 +1,104 @@
 require_relative './classes/item'
+require_relative './classes/book'
+require_relative './classes/label'
+require 'json'
+require 'fileutils'
 
-def print_menu
-  puts 'Select an option:'
-  puts '1. Create a new item'
-  puts '2. Archive an item'
-  puts '3. Quit'
-  print 'Enter your choice: '
+DATA_DIR = 'data'.freeze
+BOOKS_FILE = "#{DATA_DIR}/books.json".freeze
+LABELS_FILE = "#{DATA_DIR}/labels.json".freeze
+
+FileUtils.mkdir_p(DATA_DIR)
+
+def list_books(books)
+  puts 'Listing all books:'
+  books.each do |book|
+    puts "Title: #{book.title}, Publisher: #{book.publisher}, Published Date: #{book.publish_date}"
+  end
+  puts
 end
 
-def create_item
-  print 'Enter the publish date (YYYY-MM-DD): '
-  publish_date = gets.chomp
-  item = Item.new(Date.parse(publish_date))
-  puts "Item with ID #{item.id} created."
+def list_labels(labels)
+  puts 'Listing all labels:'
+  labels.each do |label|
+    puts "Label: #{label.title}, Color: #{label.color}"
+  end
+  puts
 end
 
-def archive_item
-  print 'Enter the publish date of the item to be archived (YYYY-MM-DD): '
+def add_book(books)
+  puts 'Adding a new book:'
+  print 'Title: '
+  title = gets.chomp
+  print 'Author: '
+  gets.chomp
+  print 'Publisher: '
+  publisher = gets.chomp
+  print 'Published Date (YYYY-MM-DD): '
   publish_date = gets.chomp
-  item = Item.new(Date.parse(publish_date))
-  item.move_to_archive
+  print 'Cover State: '
+  cover_state = gets.chomp
+
+  book = Book.new(Date.parse(publish_date), publisher, cover_state)
+  book.title = title
+  books << book
+
+  puts 'Book added successfully!'
+  puts
+end
+
+books = []
+labels = []
+
+if File.exist?(BOOKS_FILE)
+  books_data = JSON.parse(File.read(BOOKS_FILE))
+  books_data.each do |book_data|
+    book = Book.new(Date.parse(book_data['publish_date']), book_data['publisher'], book_data['cover_state'],
+                    book_data['id'])
+    book.genre = book_data['genre']
+    book.author = book_data['author']
+    book.source = book_data['source']
+    book.label = book_data['label']
+    book.archived = book_data['archived']
+    books << book
+  end
+end
+
+if File.exist?(LABELS_FILE)
+  labels_data = JSON.parse(File.read(LABELS_FILE))
+  labels_data.each do |label_data|
+    label = Label.new(label_data['id'], label_data['title'], label_data['color'])
+    label.items = label_data['items']
+    labels << label
+  end
 end
 
 loop do
-  print_menu
+  puts 'Hello! What would you like to do?'
+  puts '1. List all books'
+  puts '2. List all labels'
+  puts '3. Add a book'
+  puts '4. Quit'
+  print 'Enter your choice: '
+
   choice = gets.chomp.to_i
+
   case choice
   when 1
-    create_item
+    list_books(books)
   when 2
-    archive_item
+    list_labels(labels)
   when 3
-    puts 'Exiting the app. Goodbye!'
+    add_book(books)
+  when 4
+    books_data = books.map(&:to_hash)
+    labels_data = labels.map(&:to_hash)
+    File.write(BOOKS_FILE, JSON.generate(books_data))
+    File.write(LABELS_FILE, JSON.generate(labels_data))
+    puts 'Thank you for using app! Have a Good Day!.'
     break
   else
     puts 'Invalid choice. Please try again.'
+    puts
   end
-  puts '-' * 20
 end
