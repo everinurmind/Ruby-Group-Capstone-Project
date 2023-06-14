eafb47fb873abc9bd235f8f56ad6c970384ad239
everinurmind <nurbol.sultanov@gmail.com>
2023-06-14 03:27:24 -0600

    Add tasks 2, 3, 4
    [1pt] Implement methods (group task)
    [2pt] All Item class properties visible in the diagram should be defined and set up in the constructor method (group taks)
    [2pt] Add all methods visible in the diagram (group task)

--- classes/item.rb
+++ classes/item.rb
@@ -1,3 +1,27 @@
 class Item
-        
+  attr_reader :archive, :id
+  attr_accessor :genre, :author, :source, :label, :publish_date
+
+  def initialize(publish_date, id = nil)
+    @id = id || Random.rand(1..300)
+    @genre = nil
+    @author = nil
+    @source = nil
+    @label = nil
+    @publish_date = publish_date
+    @archived = false
+  end
+
+  def can_be_archived?
+    publish_date <= 10.years.ago.to_date
+  end
+
+  def move_to_archive
+    if can_be_archived?
+      @archived = true
+      puts 'Item has been archived'
+    else
+      puts 'Item cannot be archived'
     end
+  end
+end
