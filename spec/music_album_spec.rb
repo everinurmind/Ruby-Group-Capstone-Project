7d12340e8fcf26be1376c0e9b4ce82ebd30c5342
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2023-06-16 14:27:38 +0430

    Update music album test

--- spec/music_album_spec.rb
+++ spec/music_album_spec.rb
@@ -1,38 +1,36 @@
 require_relative '../classes/music_album'
-
-describe MusicAlbum do
-  describe '#initialize' do
-    before(:each) do
-      @album = MusicAlbum.new(true, Date.new(2012, 5, 31), false, 'Title')
-    end
-
-    it 'creates a new instance of MusicAlbum' do
-      expect(@album).to be_an_instance_of(MusicAlbum)
-    end
-
-    it 'should have attributes' do
-      expect(@album).to have_attributes(on_spotify: true, publish_date: Date.new(2012, 5, 31), archived: false,
-                                        title: 'Title')
+RSpec.describe MusicAlbum do
+  describe '#to_hash' do
+    it 'returns the music album as a hash' do
+      album = MusicAlbum.new(Date.new(2022, 1, 1), 'Artist', true, 1)
+      album.title = 'Album Title'
+      album.genre = double('Genre', id: 1)
+      album.archived = true
+      hash = album.to_hash
+      expect(hash).to be_a(Hash)
+      expect(hash[:id]).to eq(1)
+      expect(hash[:label]).to be_nil
+      expect(hash[:publish_date]).to eq(Date.new(2022, 1, 1))
+      expect(hash[:archived]).to eq(true)
+      expect(hash[:artist]).to eq('Artist')
+      expect(hash[:on_spotify]).to eq(true)
+      expect(hash[:genre]).to eq(1)
     end
   end
-
   describe '#can_be_archived?' do
-    it 'returns true if the album is on spotify and can be archived' do
-      album = MusicAlbum.new(true, Date.new(2012, 5, 31), false, 'Title')
-      can_be_archived = album.can_be_archived?
-      expect(can_be_archived).to eq(true)
+    context 'when the music album can be archived' do
+      it 'returns true' do
+        album = MusicAlbum.new(Date.new(2010, 1, 1), 'Artist', true)
+        album.on_spotify = true
+        expect(album.can_be_archived?).to eq(true)
+      end
     end
-
-    it 'returns false if the album is not on spotify' do
-      album = MusicAlbum.new(false, Date.new(2012, 5, 31), false, 'Title')
-      can_be_archived = album.can_be_archived?
-      expect(can_be_archived).to eq(false)
-    end
-
-    it 'returns false if the album cannot be archived' do
-      album = MusicAlbum.new(true, Date.new(2014, 5, 31), false, 'Title')
-      can_be_archived = album.can_be_archived?
-      expect(can_be_archived).to eq(false)
+    context 'when the music album cannot be archived' do
+      it 'returns false' do
+        album = MusicAlbum.new(Date.new(2022, 1, 1), 'Artist', true)
+        album.on_spotify = false
+        expect(album.can_be_archived?).to eq(false)
+      end
     end
   end
 end
