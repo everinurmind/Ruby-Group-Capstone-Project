617a4bae0268d7aa1e17201dcf79b8f3649a12e7
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2023-06-16 14:29:13 +0430

    Update genre spec tests

--- spec/genre_spec.rb
+++ spec/genre_spec.rb
@@ -1,38 +1,32 @@
 require_relative '../classes/genre'
-require_relative '../classes/item'
-
-describe Genre do
-  before(:each) do
-    @genre = Genre.new('Rock')
-    @item = Item.new
-  end
-
-  describe '#initialize' do
-    it 'creates a new instance of Genre' do
-      expect(@genre).to be_an_instance_of(Genre)
-    end
-
-    it 'should have attributes' do
-      expect(@genre).to have_attributes(name: 'Rock', items: [])
-    end
-  end
-
+RSpec.describe Genre do
   describe '#add_item' do
-    before(:each) do
-      @genre.add_item(@item)
-    end
-
-    it 'adds an item to the genre items collection' do
-      expect(@genre.items).to include(@item)
-    end
-
-    it 'does not add an item to the genre if it is already there' do
-      @genre.add_item(@item)
-      expect(@genre.items).to eq([@item])
-    end
-
-    it 'sets the genre of the item instance' do
-      expect(@item.genre).to eq(@genre)
+    it 'adds an item to the genre' do
+      genre = Genre.new(1, 'Science Fiction')
+      item = double('Item')
+      expect(item).to receive(:genre=).with(genre)
+      genre.add_item(item)
+      expect(genre.items).to include(item)
     end
   end
-end
+  describe '#to_hash' do
+    it 'returns the genre as a hash' do
+      genre = Genre.new(1, 'Science Fiction')
+      hash = genre.to_hash
+      expect(hash).to be_a(Hash)
+      expect(hash[:id]).to eq(1)
+      expect(hash[:name]).to eq('Science Fiction')
+      expect(hash[:items]).to eq([])
+    end
+  end
+  describe '.all_genres' do
+    it 'displays all genres' do
+      genre1 = Genre.new(1, 'Science Fiction')
+      genre2 = Genre.new(2, 'Fantasy')
+      genres = [genre1, genre2]
+      expect do
+        Genre.all_genres(genres)
+      end.to output("Listing all genres:\nGenre: Science Fiction\nGenre: Fantasy\n\n").to_stdout
+    end
+  end
+end 
